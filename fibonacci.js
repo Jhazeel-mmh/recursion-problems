87b506b2ee23354faccc5803ea28a20110bac562
Jhazeel Hernandez <140919471+Jhazeel-mmh@users.noreply.github.com>
2025-01-11 11:03:33 -0600

    add fibonacciRecursive()

--- fibonacci.js
+++ fibonacci.js
@@ -10,9 +10,23 @@ function fibonacci(n) {
   for (let i = 2; i < n - 1; i++) {
     sequence.push(fib1 + fib2);
     fib1 = sequence[i];
-    fib2 = sequence[i+1];
+    fib2 = sequence[i + 1];
   }
   return sequence;
 }
 
-console.log(fibonacci(10));
+console.log(fibonacci(8));
+
+function fibonacciRecursive(n, acc = [0,1]) {
+  // n = 1 -> [0]
+  // n = 2 -> [0,1]
+  // n = 3 -> [0,1,1]
+  if (acc.length -1 >= n) {
+    return acc.slice(0,n);
+  } else {
+    const nextNumber = acc[acc.length - 1] + acc[acc.length - 2]
+    return fibonacciRecursive(n, [...acc, nextNumber])
+  }
+}
+
+console.log(fibonacciRecursive(5));
